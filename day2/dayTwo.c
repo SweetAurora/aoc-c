bd35d718f9ad129b2024193db3eb4639539da503
Aurora <sweetaurxra@gmail.com>
2022-12-16 13:53:37 +0100

    Finished day 2

--- day2/dayTwo.c
+++ day2/dayTwo.c
@@ -2,54 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 
+const int valueTable[3] = {
+        1,
+        2,
+        3
+};
+
+const int pointsTable[3][3] = {
+        {3, 6, 0},
+        {0, 3, 6},
+        {6, 0, 3}
+};
+
+const int resultTable[3] = {
+        0, 3, 6
+};
+
+int findIndex(int resultIndex, int opponentIndex) {
+    int value = resultTable[resultIndex];
+
+    for (int i = 0; i < sizeof pointsTable[opponentIndex]; i++) {
+        if (pointsTable[opponentIndex][i] == value)
+            return i;
+    }
+    return -1;
+}
+
 int main() {
     FILE *file = fopen("./day2/input.txt", "r");
 
     int points = 0;
+    int pointsTwo = 0;
     char buffer[BUFSIZ] = {"\0"};
 
     while (fgets(buffer, sizeof(buffer), file)) {
         char *string = buffer;
         unsigned char opponent = string[0];
         unsigned char me = string[2];
-        int myCharID = me - 21;
-        int resulting = myCharID - opponent;
+        int opponentCharId = opponent - 'A';
+        int myCharID = me - 'X';
 
-        switch (resulting) {
-            case 0: //We win (c = 3||scissors vs x-21 = 3||rock)
-                points += 6;
-                break;
-            case 1: //We lose (b = 2||paper vs x-21 = 3||rock & c = 3||scissors vs y-21 = 4||paper), do nothing
-                break;
-            case 2: //Equal, we get 3 points
-                points += 3;
-                break;
-            case 3: //We win (a = 1||rock vs y-21 = 4||paper & b = 2||paper vs z-21 = 5||scissors)
-                points += 6;
-                break;
-            case 4: //We lose (a = 1||rock vs z-21 = 5||scissors), do nothing
-                break;
-            default:
-                printf("Unknown resulting value: %d", resulting);
-                return 0;
-        }
+        points += valueTable[myCharID];
+        points += pointsTable[opponentCharId][myCharID];
 
-        switch (me) {
-            case 'X':
-                points += 1;
-                break;
-            case 'Y':
-                points += 2;
-                break;
-            case 'Z':
-                points += 3;
-                break;
-            default:
-                printf("Invalid character found: %c", me);
-                return 0;
-        }
+        int newCharID = findIndex(myCharID, opponentCharId);
+        pointsTwo += valueTable[newCharID];
+        pointsTwo += resultTable[myCharID];
     }
 
-    printf("Part one: %d", points);
+    printf("Part one: %d\n", points);
+    printf("Part two: %d", pointsTwo);
+    pclose(file);
     return 0;
 }
